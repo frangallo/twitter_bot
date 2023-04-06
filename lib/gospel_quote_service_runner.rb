ecaab779d86e22930cb769227a0a89cd5cac3640
Francesco Gallo <cescogallo10@gmail.com>
2023-04-05 20:09:21 -0500

    add runner

--- lib/gospel_quote_service_runner.rb
+++ lib/gospel_quote_service_runner.rb
@@ -1,3 +1,7 @@
-require_relative 'gospel_quote_service'
-
-GospelQuoteService.new.main
+namespace :schedule do
+  desc "Run the GospelQuoteService"
+  task :gospel_quote_service => :environment do
+    require_relative 'gospel_quote_service'
+    GospelQuoteService.new.main
+  end
+end
